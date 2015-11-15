4c6791a15224a98b0c55c3cdae8724f5f13ded42
Felipe <ffelixelgato92@gmail.com>
2015-11-14 20:51:53 -0700

    OnStart method loads a list of Images from our CSV file and adds it to our layout. Blue_n_white tie is not displaying

--- app/src/main/java/com/example/jacob/teamproject/Product.java
+++ app/src/main/java/com/example/jacob/teamproject/Product.java
@@ -7,21 +7,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AbsoluteLayout;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
+import static com.example.jacob.teamproject.R.drawable.blue_n_white;
+
 class Tie {
     float cost;
-    int tieType;
-    int image;
-
-    Tie(float price) {
-        cost = price;
+    String image;
+    int stock;
+    public String getName(){return image;}
+    public Tie(String[] features)
+    {
+        assert(features.length == 3);
+        image = features[0];
+        cost = Float.parseFloat(features[1]);
+        stock = Integer.parseInt(features[2]);
     }
 }
 
@@ -45,12 +54,12 @@ class Sell {
         float p1 = 50;
         float p2 = (float) 54.23;
         float p3 = (float) 43.12;
-        Tie t1 = new Tie(p1);
-        myTies.add(t1);
-        Tie t2 = new Tie(p2);
-        myTies.add(t2);
-        Tie t3 = new Tie(p3);
-        myTies.add(t3);
+        //Tie t1 = new Tie(p1);
+        //myTies.add(t1);
+        //Tie t2 = new Tie(p2);
+        //myTies.add(t2);
+        //Tie t3 = new Tie(p3);
+        //myTies.add(t3);
 
         float myTotal = calculateTotal();
 
@@ -64,6 +73,8 @@ public class Product extends AppCompatActivity {
 
     ImageView largeView;
     Product__Controller controller;
+    List<Tie> display_ties = new ArrayList<>();
+    Sell sell = new Sell();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +93,7 @@ public class Product extends AppCompatActivity {
         });
 
         //set largeview to our invisible image view
-        largeView = (ImageView)findViewById(R.id.largeView);
+        //largeView = (ImageView)findViewById(R.id.largeView);
         //read our CSV file and pass it to the controller
         InputStream inputStream = getResources().openRawResource(R.raw.products);
         controller = new Product__Controller(inputStream);
@@ -93,12 +104,66 @@ public class Product extends AppCompatActivity {
         super.onStart();
         List<String> myList = controller.items();
         for(String item : myList){
+            Tie temp = new Tie (item.split(";"));
+            display_ties.add(temp);
+        }
+        List<ImageView> images = new ArrayList<>();
 
+        for(Tie tie : display_ties){
+            ImageView newImage = new ImageView(this);
+            switch (tie.getName()){
+                //case "Black Tie":
+                    //newImage.setImageResource(R.drawable.black);
+                    //break;
+                case "Pink Tie":
+                    newImage.setImageResource(R.drawable.pink);
+                    break;
+                case "White Tie":
+                    newImage.setImageResource(R.drawable.white);
+                    break;
+                case "Purple Tie":
+                    newImage.setImageResource(R.drawable.purple);
+                    break;
+                case "Red Tie":
+                    newImage.setImageResource(R.drawable.red);
+                    break;
+                case "Blue Tie":
+                    newImage.setImageResource(R.drawable.blue);
+                    break;
+                case "Black Blue and White":
+                    newImage.setImageResource(blue_n_white);
+                    break;
+                case "Black Red and White Tie":
+                    newImage.setImageResource(R.drawable.black_red_white);
+                    break;
+            }
+            images.add(newImage);
+        }
+        RelativeLayout rl = (RelativeLayout)findViewById(R.id.background);
+        int x = 0;
+        int y = 0;
+        int i = 0;
+        for (ImageView image : images) {
+            image.setX(x);
+            image.setY(y);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(450,450);
+            image.setLayoutParams(params);
+            rl.addView(image);
+
+            if (i > 0) {
+                x += 550;
+                x %= 1100;
+
+                if (i % 2 == 0)
+                    y += 400;
+            }
+            i++;
         }
     }
 
     public void openPurchase(View view){
         Intent intent = new Intent(this, Purchase.class);
+        //intent.putExtra(sell.myTies, sell.calculateTotal());
         startActivity(intent);
     }
     public void openGallery(View view){
@@ -110,8 +175,8 @@ public class Product extends AppCompatActivity {
         largeView.setVisibility(View.VISIBLE);
         findViewById(R.id.background).setBackgroundColor(0xff444444);
         switch(view.getId()){
-            case R.id.imageView:
-            largeView.setImageResource(R.drawable.tie);
+            //case R.id.imageView:
+            //largeView.setImageResource(R.drawable.blue);
         }
     }
     public void hideImage(View view){
@@ -128,4 +193,7 @@ public class Product extends AppCompatActivity {
         listView.setAdapter(arrayAdapter);
     }
 
+    void addToCart(View view){
+                view.getId();
+    }
 }
